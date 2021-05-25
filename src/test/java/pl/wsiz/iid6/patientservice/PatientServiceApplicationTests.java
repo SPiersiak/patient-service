24ccd1ce648d8f2ae186379faf983f793f223ef9
Szymon <piersiak.szymon@gmail.com>
2021-05-25 10:28:05 +0200

    generating the project

---
+++ src/test/java/pl/wsiz/iid6/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsiz.iid6.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
