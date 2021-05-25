1c381aeeeecaa959b3853b354221f46d6ce64058
Szymon <piersiak.szymon@gmail.com>
2021-05-25 10:59:12 +0200

    fix dto, add lekService

--- src/main/java/pl/wsiz/iid6/patientservice/dto/Lek.java
+++ src/main/java/pl/wsiz/iid6/patientservice/dto/Lek.java
@@ -1,8 +1,22 @@
-package zdrowie;
+package pl.wsiz.iid6.patientservice.dto;
 
 public class Lek {
     private String nazwa;
+
+    public Lek(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
     private double cena;
     private boolean czyNaRecepte;
     private String producent;
+
+    public Lek(String nazwa, String producent) {
+        this.nazwa = nazwa;
+        this.producent = producent;
+    }
+
+    public String getNazwa(){
+        return nazwa;
+    }
 }
