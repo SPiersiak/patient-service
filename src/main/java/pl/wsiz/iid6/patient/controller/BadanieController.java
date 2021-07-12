f093de4be4f19a38cc02854706c0b53edbb24c6a
Sebastian Poweska <spoweska98@gmail.com>
2021-07-12 23:40:48 +0200

    Badanie views

--- src/main/java/pl/wsiz/iid6/patient/controller/BadanieController.java
+++ src/main/java/pl/wsiz/iid6/patient/controller/BadanieController.java
@@ -2,11 +2,13 @@ package pl.wsiz.iid6.patient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.wsiz.iid6.patient.dto.Badanie;
+import pl.wsiz.iid6.patient.dto.Lek;
 import pl.wsiz.iid6.patient.dto.Recepta;
 import pl.wsiz.iid6.patient.service.BadanieService;
 import pl.wsiz.iid6.patient.service.PatientService;
@@ -20,21 +22,24 @@ public class BadanieController {
     private BadanieService badanieService;
 
     @GetMapping("/all")
-    @ResponseBody
-    public List<Badanie> getBadanie(){
-        return badanieService.findAll();
+    public String getBadanie(final ModelMap model){
+
+        List<Badanie> badanie = badanieService.findAll();
+        model.addAttribute("badania", badanie);
+        return "badanieAll";
     }
 
     @GetMapping("/pesel")
-    @ResponseBody
-    public List<Badanie> getbadanieByPesel(@RequestParam String pesel){
-        return badanieService.findByPesel(pesel);
+    public String getBadanieByPesel(final ModelMap model, @RequestParam String pesel){
+        List<Badanie> badanie = badanieService.findByPesel(pesel);
+        model.addAttribute("badanie", badanie);
+        return "badanieByPesel";
     }
 
     @GetMapping("/typ")
-    @ResponseBody
-    public List<Badanie> getbadanieByTyp(@RequestParam String typ){
-        return badanieService.findByTyp(typ);
+    public String getBadanieByTyp(final ModelMap model, @RequestParam String typ){
+        List<Badanie> badanie = badanieService.findByTyp(typ);
+        model.addAttribute("badanie", badanie);
+        return "badanieByTyp";
     }
-
 }
