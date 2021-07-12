b420de2aa7c5542df76957775777d0a0c45ccb69
Irek <irek.przybylo@gmail.com>
2021-07-12 18:44:31 +0200

    templates patient

--- src/main/java/restController/patientControllerJSON.java
+++ src/main/java/restController/patientControllerJSON.java
@@ -7,15 +7,23 @@ import pl.wsiz.iid6.patient.dto.Pacjent;
 import pl.wsiz.iid6.patient.service.PatientService;
 
 @RestController
-@RequestMapping(value="/api")
+@RequestMapping(value="/api/patient")
 public class patientControllerJSON {
     @Autowired
     private PatientService patientService;
 
-    @PostMapping(value="nowy", produces = "application/json")
-    public Pacjent save(@RequestBody Pacjent pacjent){
-        patientService.saveNewPacjent(pacjent);
-        return pacjent;
+    @GetMapping(path="name/{lname}", produces = "application/json")
+    public String patientName(@PathVariable String lname){
+        return (patientService.findByLastName(lname)).toString();
     }
 
+    @GetMapping(path="/all", produces = "application/json")
+    public String patientAll(){
+        return (patientService.findAll()).toString();
+    }
+
+    @GetMapping(path="pesel/{pesel}", produces = "application/json")
+    public String patientPesel(@PathVariable String pesel){
+        return (patientService.findByPesel(pesel)).toString();
+    }
 }
