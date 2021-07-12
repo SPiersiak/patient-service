e6dc9b223c99f9effbaac12a4e179c032b60011b
Szymon <piersiak.szymon@gmail.com>
2021-07-12 17:53:01 +0200

    lek controller update

--- src/main/java/pl/wsiz/iid6/patient/controller/MedicineController.java
+++ src/main/java/pl/wsiz/iid6/patient/controller/MedicineController.java
@@ -1,12 +1,15 @@
 package pl.wsiz.iid6.patient.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.wsiz.iid6.patient.dto.Lek;
 import pl.wsiz.iid6.patient.service.LekService;
+
+import java.util.List;
 
 
 @Controller
@@ -16,10 +19,35 @@ public class MedicineController
     @Autowired
     private LekService lekService;
 
-    @GetMapping(path="/find")
+    @GetMapping(path = "/allMedicine")
+    public String getMedicineAll(final ModelMap model) {
+        List<Lek> leki = lekService.getAllMedicine();
+        model.addAttribute("lekiAll", leki);
+        return "lekiAll";
+    }
+
+    @GetMapping(path = "/medicineByName")
     @ResponseBody
-    public Lek getMedicine(@RequestParam String name)
-    {
-        return lekService.findLek(name);
+    public String getMedicineByNazwa(@RequestParam String nazwa){
+        return lekService.findMedicineByName(nazwa);
+    }
+
+    @GetMapping(path = "/medicineByProducent")
+    @ResponseBody
+    public String getMedicineByProducent(@RequestParam String producent){
+        return lekService.findMedicineByProducent(producent);
+    }
+
+    @GetMapping(path = "/medicineByNameWeb")
+    public String getAllByName(final ModelMap model, @RequestParam String name) {
+        List<Lek> leki = lekService.getAllMedByName(name);
+        model.addAttribute("lekiByName", leki);
+        return "lekiByName";
+    }
+    @GetMapping(path = "/medicineByProducentWeb")
+    public String getAllByProducent(final ModelMap model, @RequestParam String producent) {
+        List<Lek> leki = lekService.getAllMedByProducent(producent);
+        model.addAttribute("lekiByProducent", leki);
+        return "lekiByProducent";
     }
 }
--- src/main/java/pl/wsiz/iid6/patient/service/LekService.java
+++ src/main/java/pl/wsiz/iid6/patient/service/LekService.java
@@ -1,28 +1,51 @@
 package pl.wsiz.iid6.patient.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import  org.springframework.stereotype.Service;
 import pl.wsiz.iid6.patient.dto.Lek;
+import pl.wsiz.iid6.patient.entity.LekEntity;
+import pl.wsiz.iid6.patient.jpa.LekRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LekService
 {
-    private Lek[] leki = {
-            new Lek("Ibuprom", "Sanofi"),
-            new Lek("szczepionka", "Astra"),
-            new Lek("Paracetamol", "Polfa")};
+    @Autowired
+    private LekRepository lekRepository;
 
-    public Lek findLek(String nazwa)
-    {
-        Lek finded = new Lek("", "");
-
-        for(Lek lek: leki)
-        {
-            if (lek.getNazwa().equals(nazwa))
-            {
-                finded = lek;
-            }
+    public List<Lek> getAllMedicine() {
+        List<Lek> lista = new ArrayList<>();
+        for (LekEntity lek : lekRepository.findAll()) {
+            lista.add(new Lek(lek.getNazwa(), lek.getCena(), lek.getProducent()));
         }
-        return finded;
+        return lista;
+    }
+
+    public String findMedicineByName(String nazwa){
+        List<LekEntity> r = lekRepository.findByNazwa(nazwa);
+        return r.toString();
+    }
+    public String findMedicineByProducent(String producent){
+        List<LekEntity> rr = lekRepository.findByProducent(producent);
+        return rr.toString();
+    }
+
+    public List<Lek> getAllMedByName(String nazwa) {
+        List<Lek> lista = new ArrayList<>();
+        for (LekEntity lek : lekRepository.findByNazwa(nazwa)) {
+            lista.add(new Lek(lek.getNazwa(), lek.getCena(), lek.getProducent()));
+        }
+        return lista;
+    }
+
+    public List<Lek> getAllMedByProducent(String producent) {
+        List<Lek> lista = new ArrayList<>();
+        for (LekEntity lek : lekRepository.findByProducent(producent)) {
+            lista.add(new Lek(lek.getNazwa(), lek.getCena(), lek.getProducent()));
+        }
+        return lista;
     }
 }
 
